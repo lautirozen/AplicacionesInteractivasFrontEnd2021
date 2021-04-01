3a554197916c46536c422091faf7123bf5c67dc7
Lauti Rozen <lautirozen@gmail.com>
2021-04-01 17:26:03 -0300

    navbar actualizada

--- src/Nosotros/Nosotros.js
+++ src/Nosotros/Nosotros.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import Navigation from '../components/Navbar';
+import NavigationLog from '../components/Navbarlog';
 import { useHistory} from "react-router-dom";
 import Footer from '../components/Footer';
 function Nosotros (props){  
     console.log("En Nosotros")
     const history= useHistory();
+    var user =JSON.parse(localStorage.getItem('user'));
     const handleHistory = () => {
         history.push("/");
     }
     return (
     <div className="Nosotros">
-        <Navigation />
+         {(user===null)?
+      <Navigation />: <NavigationLog />}
       <div class="card">
         <h6 >Productos</h6>
       </div>
